30c18d8354440d920fe60f1c567443f2a4ee6a51
pezholio <pezholio@gmail.com>
2018-12-05 09:50:06 +0000

    Allow port to be specified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const path = require('path');
 
 app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/assets')))
