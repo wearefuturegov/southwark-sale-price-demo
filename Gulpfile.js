ee80d4c3c204dadfe5fe40468106237a6224d785
pezholio <pezholio@gmail.com>
2018-12-06 11:48:02 +0000

    Gulpfile scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var vueify = require('vueify')
+var envify = require('envify/custom')
 
 sass.compiler = require('node-sass');
  
@@ -23,7 +25,16 @@ gulp.task('scripts', function() {
   browserify({
     entries: 'src/index.js'
   })
+  .transform('envify', {
+    global: true,
+    'NODE_ENV': process.env.NODE_ENV
+  })
+  .transform(vueify)
   .bundle()
   .pipe(source('index.js'))
   .pipe(gulp.dest('./js'));
 });
+
+gulp.task('scripts:watch', function() {
+  gulp.watch('./src/**/*.js', ['scripts'])
+})
